98395b8146bf17a66ea9be378f3455f237680184
M Banks <montensia1@outlook.com>
2019-06-22 01:41:36 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :integer 
+    add_column :students :grade :integer 
+    add_column :students :birthday :integer
   end
 end
